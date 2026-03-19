8b5fcc84c499f359d7798ae5c28827c926d408a6
aru-01 <arifu5758@gmail.com>
2026-03-19 06:17:12 +0600

    Modify proxy api urls

--- ai_proxy/urls.py
+++ ai_proxy/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from ai_proxy import views
 from ai_proxy.views import (
-    ProductTemplateProxyView,
     ProductUploadProxyView,
     ProductListProxyView,
     ProductDetailProxyView,
